3da40721d90edabbc689814be1cf597e4936f762
Austin Kellar <akellar@ucsc.edu>
2018-10-28 15:22:59 -0700

    finished the exercise

--- static/js/index.js
+++ static/js/index.js
@@ -9,7 +9,26 @@ var app = new Vue({
         movies: []
     },
     methods: {
+        isDuplicateTitle: function() {
+            for (var i=0; i<this.movies.length; i++) {
+                if (this.movies[i].title == this.newMovieTitle) {
+                    return true;
+                }
+            }
+            return false;
+        },
         submitMovie: function() {
+            if (this.newMovieTitle == '' || this.newMovieDescription == '' || this.newMovieRating == '') {
+                alert('You must fill in all fields before submitting!');
+                return;
+            }
+
+            if (this.isDuplicateTitle()) {
+                alert(this.newMovieTitle + " is already in the database!");
+                this.newMovieTitle = this.newMovieDescription = this.newMovieRating = '';
+                return;
+            }
+
             this.movies.push({
                 title: this.newMovieTitle,
                 description: this.newMovieDescription,
